9bced2325eed2246a5301a0305c3cfb3fd8adc5d
Torsten Both <both@comyo-media.de>
2013-03-29 11:07:47 +0100

    Lektion 3 Eingabeformular

---
+++ 2/eingabe.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * comyo.media
+ * @link http://www.comyo-media.de
+ * Projekt: Training
+ * @link $ProjectUrl$
+ *
+ * @author "Torsten Both" <both@comyo-media.de>
+ * @created 29.03.13 - 11:02
+ */
+?>
+
+<html>
+<body>
+<?php echo "Guten Tag, " . $_POST["vor"] . " " . $_POST["nach"];
+?>
+
+</body>
+</html>
